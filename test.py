7e2769d5460b88ca5ae7240e938ca5ad57985d7f
vsmac <vsoodles@gmail.com>
2019-05-22 10:39:10 +0530

    add program to find second largest no. in list

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-list1=[] 
-for i in range (10):
-    x=int(input("enter no. \n")) 
-    list1.append(x)
-print(list1) 
+#!/usr/bin/python3
+
+list = [12,45,555555,67,89,444444444444444,444444455,111]
+x = 0
+y = 0
+list1 = []
+for i in list:
+   if i > x:
+     x = i
+     list.remove(x)
+for j in list:
+    if j > y:
+       y = j
+print(y)
+print(x)
